3ae20ab1b3aeb42e41e6f363f249305412d53250
Sudheer-Pilli <sudheeer.p@mca.christuniversity.in>
2024-01-19 23:46:00 +0530

    node_modules
    Sorry i made a spelling mistake

--- firstnode.js
+++ firstnode.js
@@ -1,8 +1,22 @@
-var http = require("http")
+// var http = require("http")
 
-http.createServer(function(req,res){
+const { request } = require("http")
 
-    res.writeHead(200,{'content-Type':'text/html'})
-    res.write("Hey Node")
-    res.end()
-}).listen(8080);
+// http.createServer(function(req,res){
+
+//     res.writeHead(200,{'content-Type':'text/html'})
+//     res.write("Hey Node")
+//     res.end()
+// }).listen(8080);
+
+// var http = require('http')
+
+// http.createServer(function(req,res){
+//     res.write("Hey");
+//     res.end()
+// }).listen('8080')
+
+var cals = require('./making_modlues.js')
+
+result = cals.add(2,2)
+console.log(result)
