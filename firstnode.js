729ff78aee704b2dee3fc3d0828b05a0b1684d1a
Sudheer-Pilli <sudheeer.p@mca.christuniversity.in>
2024-01-20 15:50:55 +0530

    Update firstnode.js

--- firstnode.js
+++ firstnode.js
@@ -1,6 +1,5 @@
 // var http = require("http")
 
-const { request } = require("http")
 
 // http.createServer(function(req,res){
 
@@ -16,7 +15,63 @@ const { request } = require("http")
 //     res.end()
 // }).listen('8080')
 
-var cals = require('./making_modlues.js')
+// var http = require("http")
 
-result = cals.add(2,2)
-console.log(result)
+// var cals = require('./making_modlues.js')
+
+// result = cals.add(2,2)
+// console.log(result)
+
+
+// http.createServer(function (req,res){
+//     res.write("Hey My output is ",result)
+//     console.log(result)
+//     res.end()
+// }).listen("8080")
+
+// var file_system = require("fs")
+
+// file_system.unlink('writing_file.js', function(err){
+//     console.log("Its deleted")
+// })
+
+// const express = require("express");
+
+
+// const firstnode = express();
+
+// firstnode.get('/',function(req,res){
+//     res.send("Hello world")
+// })
+
+// firstnode.get("/username", function(req,res){
+//     res.send("Welcome back Sudheer")
+
+// })
+
+// firstnode.listen(9000, function(req,res){
+//     console.log("Running...")
+// });
+
+
+const express = require('express')
+
+const firstnode = express()
+
+firstnode.listen(9000, function(req,res){
+    console.log("Running")
+})
+
+firstnode.get("/",function(req,res){
+    res.send("My Server is running")
+})
+
+firstnode.get("/user/:id", function(req,res){
+    const id = req.params.id
+    res.send("hey user no " + id);
+})
+
+firstnode.get('/user', function(req,res){
+    const id = req.query.id
+    res.send("welcome user and no" + id)
+})
